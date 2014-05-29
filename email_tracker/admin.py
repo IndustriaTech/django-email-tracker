95223eaa3c0f1d34d2c3f43ffc6b521a38b3d107
Venelina Yanakieva <vili@magicsolutions.bg>
2014-05-29 18:25:58 +0300

    Add date filter

--- email_tracker/admin.py
+++ email_tracker/admin.py
@@ -6,7 +6,7 @@ from email_tracker.models import EmailCategory, TrackedEmail
 class TrackedEmailAdmin(admin.ModelAdmin):
     list_filter = ('category', )
     readonly_fields = ['subject', 'from_email', 'recipients', 'cc', 'bcc', 'body', 'is_sent', 'category', 'created_at']
- 
+    date_hierarchy = 'created_at'
 
 admin.site.register(TrackedEmail, TrackedEmailAdmin)
 admin.site.register(EmailCategory)
