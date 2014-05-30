25b97441a042640b342cb92d64dad3c560a6af79
Venelina Yanakieva <vili@magicsolutions.bg>
2014-05-30 13:27:43 +0300

    Add admin filters and columns

--- email_tracker/admin.py
+++ email_tracker/admin.py
@@ -4,7 +4,8 @@ from email_tracker.models import EmailCategory, TrackedEmail
 
 
 class TrackedEmailAdmin(admin.ModelAdmin):
-    list_filter = ('category', )
+    list_filter = ('category', 'created_at')
+    list_display = ('__unicode__', 'created_at')
     readonly_fields = ['subject', 'from_email', 'recipients', 'cc', 'bcc', 'body', 'is_sent', 'category', 'created_at']
     date_hierarchy = 'created_at'
 
