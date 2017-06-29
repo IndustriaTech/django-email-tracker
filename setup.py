f12d4b3d18aab5b0dcf80f7e9b6084e36a67fbc0
Venelin Stoykov <venelin@magicsolutions.bg>
2017-06-29 15:23:14 +0300

    Version 0.5.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def get_long_description():
 
 setup(
     name='django-email-tracker',
-    version='0.5.1',
+    version='0.5.2',
     description='Email Tracker for Django',
     author='Venelina Yanakieva',
     author_email='vili@magicsolutions.bg',
